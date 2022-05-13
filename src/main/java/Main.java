5283831453cd5ce96e74ed0921a947c931e40f7c
RuzaikinDY <Devilwind@mail.ru>
2022-05-13 13:34:09 +0300

    Reformat code

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,15 +1,15 @@
 public class Main {
-  public static void main(String[] args) {
-    BonusService service = new BonusService();
+    public static void main(String[] args) {
+        BonusService service = new BonusService();
 
-    long amount = 1000_60;
-    boolean registered = true;
-    long expected= 30;
+        long amount = 1000_60;
+        boolean registered = true;
+        long expected = 30;
 
-    long bonuspoint = service.calculate(amount, registered);
-    boolean good = expected == bonuspoint;
+        long bonuspoint = service.calculate(amount, registered);
+        boolean good = expected == bonuspoint;
 
-    System.out.println(good);
-  }
+        System.out.println(good);
+    }
 }
 
